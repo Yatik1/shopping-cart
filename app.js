789c30c4f7bece3957936689bc3640bff00506a0
Yatik1 <yatiksrivastava1@gmail.com>
2024-08-30 13:51:17 +0530

    dbconnect

--- app.js
+++ app.js
@@ -2,11 +2,17 @@ const express = require("express")
 const app = express()
 
 const cookieParser = require("cookie-parser")
+const dotenv = require("dotenv")
+const dbConnect = require("./config/dbConnect")
+
+dotenv.config()
+
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
 
+dbConnect()
 
 app.get("/" , (req,res) => {
     res.send("welcome to the home route")
