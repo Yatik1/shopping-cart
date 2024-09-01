6cea44b5e07a6d884b37bf9de387062fe418e998
Yatik1 <yatiksrivastava1@gmail.com>
2024-09-01 12:43:15 +0530

    app

--- app.js
+++ app.js
@@ -5,23 +5,31 @@ const cookieParser = require("cookie-parser")
 const dotenv = require("dotenv")
 const dbConnect = require("./config/dbConnect")
 const dbgr = require("debug")("development:app")
+const expressSession = require("express-session")
+const flash = require("flash")
 
+const indexRouter = require("./routes/index.routes")
 const ownersRouter = require("./routes/owners.routes")
 const productsRouter = require("./routes/products.routes")
 const usersRouter = require("./routes/users.routes")
 
 dotenv.config()
 
-
+  
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
+app.use(expressSession({
+    resave:false,
+    saveUninitialized:false,
+    secret : process.env.EXPRESS_SECRET_KEY
+}))
+
+app.use(flash())
 
 dbConnect()
 
-app.get("/" , (req,res) => {
-    res.send("welcome to the home route")
-})
+app.use("/",indexRouter)
 
 app.use("/owner" , ownersRouter)
 app.use("/user",usersRouter)
