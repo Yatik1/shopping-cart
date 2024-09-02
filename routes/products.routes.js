9f26a6a4ed9dd3c7e37b3a12e222a59ffb5722d0
Yatik1 <yatiksrivastava1@gmail.com>
2024-09-02 15:52:56 +0530

    product route

--- routes/products.routes.js
+++ routes/products.routes.js
@@ -1,8 +1,34 @@
 const express = require("express")
 const router = express.Router()
+const upload = require("../config/multerConfig")
+const productModel = require("../models/product.model")
 
 router.get("/" , (req,res) => {
     res.send("This productss router")
+})
+
+router.post("/create", upload.single("image") , async (req,res) => {
+   try {
+        let {name , price , discount, bgColor , panelColor , textColor} = req.body
+
+        let product = await productModel.create({
+            image:req.file.buffer,
+            name,
+            price,
+            discount,
+            bgColor,
+            panelColor,
+            textColor
+        })
+ 
+        res.send(product)
+
+        req.flash("success" , "Product created Successfully")
+        res.redirect("/owner/admin")
+
+   } catch (error) {
+        res.send("Error occured in creating product" , error)
+   }
 })
 
 module.exports = router
