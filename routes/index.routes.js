03aaa714c0c14ee192336aaaadd277d9874a2950
Yatik1 <yatiksrivastava1@gmail.com>
2024-09-03 14:12:22 +0530

    route updated

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -3,15 +3,39 @@ const router = express.Router()
 
 const isLoggedIn = require("../middlewares/isLoggedIn")
 const productModel = require("../models/product.model")
+const userModel = require("../models/user.model")
 
 router.get("/" , (req,res) => {
     let error = req.flash("error")
-    res.render("index" , {error})
+    res.render("index" , {error  , loggedin : false })
 })
 
 router.get("/shop" , isLoggedIn , async (req,res) => {
     let products = await productModel.find()
-    res.render("shop" , {products})
+    let success = req.flash('success') 
+    res.render("shop" , {products , success})
+})
+
+router.get("/addtocart/:productid" , isLoggedIn , async (req,res) => {
+    let user = await userModel.findOne({email:req.user.email})
+
+    user.cart.push(req.params.productid)
+    await user.save()
+
+    req.flash('success' , "Added to Cart")
+    res.redirect("/shop")
+
+})
+
+router.get("/cart" , isLoggedIn , async(req,res) => {
+    let user = userModel.findOne({email : req.user.email}).populate("cart")
+
+    res.render("cart" , {user})
+
+})
+
+router.get("/logout" , isLoggedIn , (req,res) => {
+    res.render("shop")
 })
 
 module.exports = router
